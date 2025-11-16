8772ab9c9d956575a3fbb9ec2a244c846a88fb3d
riteofrene <riteofrenaissance@gmail.com>
2025-11-16 08:06:53 +0100

    Delete docs/conf.py

--- docs/conf.py
+++
@@ -1,9 +0,0 @@
-git clone --depth 1 https://github.com/riteofrenaissance/ecf-compass.git . 
-git fetch origin --force --prune --prune-tags --depth 50 refs/heads/main:refs/remotes/origin/main 
-git checkout --force origin/main 
-cat readthedocs.yaml 
-asdf global python 3.13.3 
-python -mvirtualenv $READTHEDOCS_VIRTUALENV_PATH 
-python -m pip install --upgrade --no-cache-dir pip setuptools 
-python -m pip install --upgrade --no-cache-dir sphinx 
-python -m sphinx -T -b html -d _build/doctrees -D language=en . $READTHEDOCS_OUTPUT/html
