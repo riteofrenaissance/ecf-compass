57f01d18b65126324f79738fd2e534b3b51d2409
riteofrene <riteofrenaissance@gmail.com>
2025-11-16 17:54:28 +0100

    Add files via upload

---
+++ docs/conf.py
@@ -0,0 +1,17 @@
+# docs/conf.py
+project = 'ECF Compass'
+copyright = '2024, Rite of Renaissance'
+author = 'Samir Baladi'
+
+extensions = [
+    'myst_parser',
+    'sphinx.ext.autodoc',
+]
+
+html_theme = 'sphinx_rtd_theme'
+
+# دعم ملفات Markdown
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.md': 'markdown',
+}
